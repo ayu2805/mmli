468542c0ce114580f1a7f201afbe5e43291a8fec
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2025-05-05 14:46:37 +0530

    Update minecraftmodloader.ps1

--- minecraftmodloader.ps1
+++ minecraftmodloader.ps1
@@ -16,29 +16,29 @@ if (-not (Check-Java)) {
 }
 
 function Install-Forge {
-  if ($(Get-Location).Path -notlike "*\forge-server") {
-    New-Item -Path "forge-server" -ItemType Directory -Force
-    Set-Location -Path "forge-server"
-  }
   $url = "https://files.minecraftforge.net/net/minecraftforge/forge/"
   $htmlContent = Invoke-WebRequest -Uri $url -UseBasicParsing -ErrorAction Stop
   $targetUrl = ($htmlContent.Content -match 'https://adfoc.us/serve/sitelinks/\?id=271228&url=([^"]*)') | Out-Null
   $targetUrl = $matches[1]
-  Invoke-WebRequest -Uri $targetUrl -OutFile "forge.jar"
+  Invoke-WebRequest -Uri $targetUrl -OutFile "forge-installer.jar"
   if (Test-Path -Path $env:APPDATA\.minecraft -PathType Container) {
-    Invoke-Expression "java -jar forge.jar --installClient $env:APPDATA\.minecraft"
+    & java -jar forge.jar --installClient $env:APPDATA\.minecraft
   }
-  Invoke-Expression "java -jar forge.jar --installServer"
-  Remove-Item "forge.jar", "forge.jar.log" -ErrorAction SilentlyContinue
+  $confirmRegistry = Read-Host "Do you want to install Fabric Server? (y/N)"
+  if ($confirmRegistry -match '^(yes|y)$') {
+    if ($(Get-Location).Path -notlike "*\forge-server") {
+      New-Item -Path "forge-server" -ItemType Directory -Force
+      Move-Item -Path .\forge-installer.jar -Destination .\forge-server\
+      Set-Location -Path "forge-server"
+    }
+    & java -jar forge-installer.jar --installServer
+    Set-Location -Path ..
+  }
+  Remove-Item "forge-installer.jar", "forge-installer.jar.log" -ErrorAction SilentlyContinue
   Invoke-Expression "./run.bat --initSettings"
-  Set-Location -Path ..
 }
 
 function Install-Fabric {
-  if ($(Get-Location).Path -notlike "*\fabric-server") {
-    New-Item -Path "fabric-server" -ItemType Directory -Force
-    Set-Location -Path "fabric-server"
-  }
   $metadataUrl = "https://maven.fabricmc.net/net/fabricmc/fabric-installer/maven-metadata.xml"
   $baseJarUrl = "https://maven.fabricmc.net/net/fabricmc/fabric-installer"
   $xml = [xml](Invoke-WebRequest -Uri $metadataUrl -UseBasicParsing -ErrorAction Stop).Content
@@ -46,12 +46,23 @@ function Install-Fabric {
   $jarUrl = "$baseJarUrl/$latestVersion/fabric-installer-$latestVersion.jar"
   Invoke-WebRequest -Uri $jarUrl -OutFile "fabric-installer.jar" -UseBasicParsing -ErrorAction Stop
   if (Test-Path -Path $env:APPDATA\.minecraft -PathType Container) {
-    Invoke-Expression "java -jar .\fabric-installer.jar client"
+    & java -jar .\fabric-installer.jar client
   }
-  Invoke-Expression "java -jar .\fabric-installer.jar server -downloadMinecraft"
-  Invoke-Expression "java -jar .\fabric-server-launch.jar --initSettings"
+  $confirmRegistry = Read-Host "Do you want to install Fabric Server? (y/N)"
+  if ($confirmRegistry -match '^(yes|y)$') {
+    if ($(Get-Location).Path -notlike "*\fabric-server") {
+      New-Item -Path "fabric-server" -ItemType Directory -Force
+      Move-Item -Path .\fabric-installer.jar -Destination .\fabric-server\
+      Set-Location -Path "fabric-server"
+    }
+    & java -jar .\fabric-installer.jar server -downloadMinecraft
+    & java -jar .\fabric-server-launch.jar --initSettings
+    Remove-Item "fabric-installer.jar" -ErrorAction SilentlyContinue
+    Set-Location -Path ..
+  }
+  & java -jar .\fabric-installer.jar server -downloadMinecraft
+  & java -jar .\fabric-server-launch.jar --initSettings
   Remove-Item "fabric-installer.jar" -ErrorAction SilentlyContinue
-  Set-Location -Path ..
 }
 
 do {
