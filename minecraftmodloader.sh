4772c7f19f05ba6efd5e358534dde3556dbace08
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2025-05-05 15:00:04 +0530

    Update minecraftmodloader.sh

--- minecraftmodloader.sh
+++ minecraftmodloader.sh
@@ -9,42 +9,65 @@ check_java() {
 }
 
 install_forge() {
-  if [[ ! "$(pwd)" == *"forge-server"* ]]; then
-    mkdir -p "forge-server"
-    cd "forge-server"
-  fi
   url="https://files.minecraftforge.net/net/minecraftforge/forge/"
   target_url=$(curl -s "$url" | \
             grep -o 'https://adfoc.us/serve/sitelinks/?id=271228&url=[^"]*' | \
             head -n 1 | \
             sed 's|https://adfoc.us/serve/sitelinks/?id=271228&url=||')
-  curl -o "forge.jar" "$target_url"
-  if [ -d "$HOME/.minecraft" ]; then
-    java -jar forge.jar --installClient "$HOME/.minecraft"
+  curl -o "forge-installer.jar" "$target_url"
+  
+  read -r -p "Do you want to install Forge Client? (y/N)" response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if [ -d "$HOME/.minecraft" ]; then
+    java -jar forge-installer.jar --installClient "$HOME/.minecraft"
+    fi
   fi
-  java -jar forge.jar --installServer
-  rm -f forge.jar forge.jar.log
-  ./run.sh --initSettings
-  cd ..
+  
+  read -r -p "Do you want to install Forge Server? (y/N)" response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if [[ ! "$(pwd)" == *"forge-server"* ]]; then
+      mkdir -p "forge-server"
+      mv "forge-installer.jar" "forge-server"
+      cd "forge-server"
+    fi
+    java -jar forge-installer.jar --installServer
+    ./run.sh --initSettings
+    rm -f forge-installer.jar forge-installer.jar.log
+    cd ..
+  fi
+  
+  rm -f forge-installer.jar forge-installer.jar.log
 }
 
 install_fabric() {
-  if [[ ! "$(pwd)" == *"fabric-server"* ]]; then
-    mkdir -p "fabric-server"
-    cd "fabric-server"
-  fi
   metadata_url="https://maven.fabricmc.net/net/fabricmc/fabric-installer/maven-metadata.xml"
   base_jar_url="https://maven.fabricmc.net/net/fabricmc/fabric-installer"
   latest_version=$(curl -s "$metadata_url" | grep -oP '<latest>\K[^<]+')
   jar_url="$base_jar_url/$latest_version/fabric-installer-$latest_version.jar"
   curl "$jar_url" -o "fabric-installer.jar"
-  if [ -d "$HOME/.minecraft" ]; then
-    java -jar fabric-installer.jar client
+  
+  read -r -p "Do you want to install Fabric Client? (y/N)" response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if [ -d "$HOME/.minecraft" ]; then
+      java -jar fabric-installer.jar client
+    fi
   fi
-  java -jar fabric-installer.jar server -downloadMinecraft
-  java -jar fabric-server-launch.jar --initSettings
+  
+  read -r -p "Do you want to install Fabric Server? (y/N)" response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if [[ ! "$(pwd)" == *"fabric-server"* ]]; then
+      mkdir -p "fabric-server"
+      mv "fabric-installer.jar" "fabric-server"
+      cd "fabric-server"
+    fi
+    java -jar fabric-installer.jar server -downloadMinecraft
+    java -jar fabric-server-launch.jar --initSettings
+    rm -f fabric-installer.jar
+    cd ..
+  fi
+  
   rm -f fabric-installer.jar
-  cd ..
+
 }
 
 while true; do
